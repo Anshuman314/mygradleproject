ae6f369a1f5ff95b24b926c2ef908e21de4373a4
Anshuman <73136987+Anshuman314@users.noreply.github.com>
2024-08-08 18:32:42 +0530

    Create main1.java

---
+++ main1.java
@@ -0,0 +1,18 @@
+private static String render(Map<String, Object> model, String templatePath) throws IOException {
+        try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(templatePath)) {
+            if (inputStream == null) {
+                throw new FileNotFoundException("Resource not found: " + templatePath);
+            }
+            
+            // Alternative way to read all bytes from an InputStream
+            ByteArrayOutputStream result = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) != -1) {
+                result.write(buffer, 0, length);
+            }
+
+            // Return the content as a string
+            return result.toString(StandardCharsets.UTF_8.name());
+        }
+    }
